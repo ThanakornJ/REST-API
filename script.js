db3b7b8e3b2d6b4682c637c85f66d20952cdf271
ThanakornJ <84006233+ThanakornJ@users.noreply.github.com>
2021-06-20 02:08:39 +0700

    $git log
    1.ทำการเพิ่มข้อมูล
    2.แก้ไช่ข้อมูลในฟังก์ชี่น
    3.onload url ใหม่

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* ดึงข้อมูลผ่าน POST MAN */
+
+// นักเรียน 1 คน
 let id = document.getElementById('id');
 let stuentId = document.getElementById('studentId');
 let nameStudent = document.getElementById('name');
@@ -12,15 +15,62 @@ function addStudentData(student) {
     img.setAttribute('src', student.image);
 }
 
+// GET ข้ อมูลจาก POSTMAN - > นั กเรียน 1 ตน
+
+// function onLoad() {
+//     fetch('https://dv-student-backend-2019.appspot.com/student')
+//         .then((response) => {
+//             return response.json()
+//         }).then(data => {
+
+//             addStudentData(data)
+//         })
 
 
+// }
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+// นักเรียนหลายคน
+
+function addStudentList(studentList) {
+    let counter = 1
+    for (student of studentList) {
+        addStudentToTable(counter++, student)
+    }
+}
+
+function addStudentToTable(index, student) {
+
+    const tableBody = document.getElementById('tableBody');
+    let row = document.createElement('tr');
+    let cell = document.createElement('th');
+    let img = document.createElement('img');
+    cell.setAttribute('score', 'row');
+    cell.innerHTML = index;
+    row.appendChild(cell);
+    cell = document.createElement('td');
+    cell.innerHTML = `${student.name}${student.surname}`;
+    row.appendChild(cell);
+    cell = document.createElement('td');
+    img.setAttribute('src', student.image);
+    img.classList.add('img-thumbnail'); // ใส้กรอกให้เฉยๆ
+    cell.appendChild(img);
+    row.appendChild(cell);
+    cell = document.createElement('td');
+    cell.innerHTML = student.gender;
+    row.appendChild(cell);
+    tableBody.appendChild(row);
+
+
+}
+// GET ข้อมูลจาก POSTMAN - > นักเรียนหลายคน
 function onLoad() {
-    fetch('https://dv-student-backend-2019.appspot.com/student')
+    fetch('https://dv-student-backend-2019.appspot.com/students')
         .then((response) => {
             return response.json()
         }).then(data => {
 
-            addStudentData(data)
+            addStudentList(data)
         })
 
 
